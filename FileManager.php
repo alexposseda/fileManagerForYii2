712d31c6b7258a7588af4e5ff25b6c0a31f3b8d1
AlexPosseda <alexposseda@gmail.com>
2016-08-18 11:33:47 +0300

    init

---
+++ FileManager.php
@@ -0,0 +1,13 @@
+<?php
+	namespace yii\alexposseda\fileManager;
+
+    /**
+     * Class FileManager
+     * @package grsolution\fileManager
+     *
+     * @property string $storagePath
+     * @property string $storageUrl;
+     */
+    class FileManager extends Object{
+        
+    }
